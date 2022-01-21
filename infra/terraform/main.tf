66d36489e7e5eff511496442b53cce49570f9183
it <ittiel@gmail.com>
2022-01-21 22:45:32 +0200

    adding arn to ecs task definition

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -211,6 +211,33 @@ resource "aws_ecs_cluster" "orca" {
 # Create a task definition
 # -----------------------------------------------------------------------------
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "role-name"
+
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+
+resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+
 locals {
 #  ecs_environment = [
 #    {
@@ -236,13 +263,14 @@ locals {
   ]
 }
 
+
 resource "aws_ecs_task_definition" "orca" {
   family                   = "orca"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-#  execution_role_arn       = aws_iam_role.orca_role.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode(local.ecs_container_definitions)
 }
