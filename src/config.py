6eb2b31a037fe850c75e5b0de2f28db0d4c64449
it <ittiel@gmail.com>
2022-01-22 11:13:38 +0200

    adding dependency psycopg2-binary

--- src/config.py
+++ src/config.py
@@ -2,5 +2,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', f'postgres://{os.path.join(basedir, "app.db")}')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', f'postgresql://{os.path.join(basedir, "app.db")}')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
